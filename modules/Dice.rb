5fd675d8d02e0646cb5bf5a9d7c1e2f652efe307
memalign <memalign@not-a-valid-address>
2012-09-09 18:30:34 -0700

    Added a Dice command

---
+++ modules/Dice.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/ruby
+#
+
+class Dice 
+    @@NAME = "Dice"
+    def Dice.name
+        return @@NAME
+    end
+
+    def parseState(line)
+        return line
+    end
+
+    def serialize(state)
+        return state
+    end
+
+    def shortDescription
+        return "dice [NdS] - roll 🎲"
+    end
+
+    def process(chatServer, user, text)
+        diceString = text.split(' ', 2)[1]
+        
+        matches = /^(\d+)d(\d+)/i.match(diceString)
+        amtDice = 1
+        numSides = 6
+        if (matches)
+            inputAmtDice = matches[1].to_i
+            if ((inputAmtDice >= 1) && (inputAmtDice <= 999))
+                amtDice = inputAmtDice
+            end
+
+            if ((numSides >= 0) && (numSides <= 999))
+                numSides = matches[2].to_i
+            end
+        end
+
+        message = "Rolled #{amtDice}d#{numSides}: "
+        sum = 0
+        comma = ""
+        sumString = ""
+        1.upto(amtDice) { |x|
+            value = 1 + rand(numSides)
+            sumString << "#{comma}#{value}"
+            sum += value
+            comma = "+"
+        }
+
+        if (amtDice > 1)
+            message << "#{sumString}=#{sum}"
+        else
+            message << "#{sum}"
+        end
+
+        chatServer.sendMessage(user.handle, message)
+    end
+end
+
+ModuleController.loadModule(Dice.name, Dice)
+
